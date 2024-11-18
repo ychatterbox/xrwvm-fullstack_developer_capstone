e074dd4cc6cee7a24e0c5d75e6a383fe71d179bb
ychatterbox <ychatterbox@gmail.com>
2024-11-18 11:25:54 -0500

    Created superuser. Added user management functionality: login, logout, and register functionalities.

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,13 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import RegisterContainer from "./components/Register/Register";
+
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+            <Route path="/register" element={<RegisterContainer />} />
     </Routes>
   );
 }
